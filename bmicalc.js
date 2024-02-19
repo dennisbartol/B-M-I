7e1ac0c55ee99b2a293dcb500b6e5a56ca4e0b73
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-19 12:39:55 +0100

    Update bmicalc.js

--- bmicalc.js
+++ bmicalc.js
@@ -1,8 +1,10 @@
-let height
-let weight 
-let calculation
-let bmiText
-let result
-let health_status
+let height = document.querySelector('.container .height input');
+let weight = document.querySelector('.container .weight input');
+let calculation = document.querySelector('container .calculate-btn');
+let bmiText = document.querySelector('.container .result-box .bmi h3');
+let result = document.querySelector('.container .result-box');
+let health_status = document.querySelector('.container .result-box .result');
 
 // let 
+
+
