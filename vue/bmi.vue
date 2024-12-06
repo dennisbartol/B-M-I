4bc3d28cda6716649da53c9bf90b4e5782866c1b
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-06 13:51:31 +0100

    Update bmi.vue

--- vue/bmi.vue
+++ vue/bmi.vue
@@ -32,7 +32,7 @@
         if (bmi < 18.5) {
           return "You\'re underweight"; 
         } else if (bmi >= 18.5 && bmi < 24.9) {
-          return "You have a normal weight"; 
+          return "You have on a regular and healthy weight"; 
         }
 
 
