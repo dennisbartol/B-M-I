a4a6240a5fa3bbd8313e8269f03e3888b24c23c3
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-10 10:39:20 +0100

    Update app.module.ts

--- angular/app.module.ts
+++ angular/app.module.ts
@@ -2,3 +2,4 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } fomr '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component'; 
+import {  } ; 
