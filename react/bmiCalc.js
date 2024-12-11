3632021be8a59362558f88f2fa54decb214a3b29
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-11 13:30:18 +0100

    Update bmiCalc.js

--- react/bmiCalc.js
+++ react/bmiCalc.js
@@ -1,4 +1,14 @@
-impot React, { useState } from 'react'; 
+import React, { useState } from 'react'; 
 import './bmistyles.css';
+
+const BMICalculator = () => {
+    const [height, setHeight] = useState(''); 
+    const [weight, setWeight] = useState('');
+    const [bmi, setBMI] = useState('');
+    const [healthStatus, setHealthStatus] = useState('');
+    const [healthStatusColor, setHealthStatusColor] = useState('');
+
+
+}
 
 
