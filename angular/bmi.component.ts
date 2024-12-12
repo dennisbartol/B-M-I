39580de433fbdb21198fbd7b85c98a288985bb63
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-12 19:46:42 +0100

    Update bmi.component.ts

--- angular/bmi.component.ts
+++ angular/bmi.component.ts
@@ -27,7 +27,7 @@ export class BmiCalculatorComponent {
   getBMIResultText(bmi: number): string { 
       if (bmi < 18.5) {
         return 'You\'re underweight'; 
-      } else if (bmi >= 18.5 && < 24.9) {
+      } else if (bmi >= 18.5 && bmi < 24.9) {
         return 'You\'re on a regular and normal weight'; 
       } else if (bmi >= 25 && bmi < 29.9) {
         return 'You\'re overweight'; 
