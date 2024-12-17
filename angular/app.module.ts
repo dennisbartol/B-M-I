0e0b1af4ed41d7fd5654ce36eb307060d97a75d5
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-17 20:58:18 +0100

    Update app.module.ts

--- angular/app.module.ts
+++ angular/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component'; 
-import { BmiCalculatorComponent } from bmi.component.ts ; 
+import { BmiCalculatorComponent } from './bmi.component.ts' ; 
 
 @NgModule({
   declarations: [
