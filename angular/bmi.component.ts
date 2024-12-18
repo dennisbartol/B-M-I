190045a88319b5c19ff1e218a3fd74c08fd8d234
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-18 22:15:38 +0100

    Update bmi.component.ts

--- angular/bmi.component.ts
+++ angular/bmi.component.ts
@@ -14,7 +14,7 @@ export class BmiCalculatorComponent {
 
 
   calculateBMI(): void{ 
-      if(this.Height !== null && this.weight) {
+      if(this.Height !== null && this weight > 0 && ) {
         const heightInMeters = this.height / 100; 
         this.bmi = parseFloat((this.weight / (heightInMeters ** 2)).toFixed(2));
         this.resultText = this.getBMIResultText(this.bmi);
