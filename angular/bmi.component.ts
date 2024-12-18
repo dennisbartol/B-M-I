d7324597c64a74f635c51795bd538dbfdf0bb75f
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-18 22:19:40 +0100

    Update bmi.component.ts

--- angular/bmi.component.ts
+++ angular/bmi.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'app-bmi-calculator',
-  templateUrl: 'bmi.component.html', 
-  styleUrls: ['bmi.component.css']
+  templateUrl: './bmi.component.html', 
+  styleUrls: ['./bmi.component.css']
 })
 
 export class BmiCalculatorComponent {
